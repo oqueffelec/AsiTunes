36b185fd61ed14fa6df0d421f1bee353674437e5
pmaksud <paul.maksud@insa-rouen.fr>
2016-05-25 14:20:44 +0200

    interface graphique v1

--- src/main/java/prof/prodageo/org/MyUI.java
+++ src/main/java/prof/prodageo/org/MyUI.java
@@ -1,3 +1,4 @@
+
 package prof.prodageo.org;
 
 import javax.servlet.annotation.WebServlet;
@@ -12,7 +13,14 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.HorizontalLayout;
+import java.util.Arrays;
+import java.util.List;
 
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.ui.ListSelect;
+import com.vaadin.ui.VerticalLayout;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +28,17 @@ import org.slf4j.LoggerFactory;
 /* import for explicit declaration of callback */
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Button.ClickEvent;
+
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+
+
+import com.vaadin.ui.TwinColSelect;
+import com.vaadin.ui.VerticalLayout;
+import java.util.* ;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -36,10 +55,14 @@ public class MyUI extends UI {
 
     /* explicit declaration as attributes of graphical components for GenMyModel */
         final VerticalLayout layout = new VerticalLayout();
-        final TextField name = new TextField();
-        final TextField surname = new TextField();
-        Button button = new Button("Click Me") ;
+        final TextField rechercheField = new TextField();
+        Button rechercher = new Button("rechercher") ;
+        private  com.vaadin.ui.TextArea editor;
+      
+        final HorizontalLayout hol = new HorizontalLayout();
+        private TwinColSelect twc ;
         
+
 
     /* explicit callback */
     /* https://vaadin.com/docs/-/part/framework/application/application-events.html */
@@ -47,10 +70,27 @@ public class MyUI extends UI {
     {
         public void buttonClick(ClickEvent event) 
         {
-            layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
-            log.info("Button clicked with value : " + name.getValue());
+        
+            layout.addComponent(new Label(rechercheField.getValue()));
         }
     }
+
+   
+
+    
+    public class gestionBarre implements Property.ValueChangeListener {
+
+
+        public void valueChange(Property.ValueChangeEvent event) 
+        {
+            String s = (String)event.getProperty().getValue();
+            layout.addComponent(new Label(s));
+
+
+        }
+
+    }
+
         
 
 
@@ -61,7 +101,7 @@ public class MyUI extends UI {
         // final VerticalLayout layout = new VerticalLayout();
         
         // final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+        rechercheField.setCaption("entrez un tag ou une musique");
 
         /*
         Button button = new Button("Click Me");
@@ -72,11 +112,46 @@ public class MyUI extends UI {
         });
         */
         ClickMeClass callback = new ClickMeClass() ;
-        button.addClickListener( callback ) ;
+        
+        layout.addComponents(rechercheField, rechercher);
+         hol.setWidth("400px");
+         hol.setWidth("400px");
+         //List<String> list= Arrays.asList(new String[] {
+           // "Berlin", "Brussels", "Helsinki", "Madrid", "Oslo", "Paris",
+           // "Stockholm" });
+         //musiqueTag = new ListSelectSingleExample(hol,"musiques recherchées",list);
+         //maPlay = new ListSelectSingleExample(hol,"  ma playlist" ,list);
 
-        layout.addComponents(name, button);
-        layout.setMargin(true);
-        layout.setSpacing(true);
+         TwinColSelect twc = new TwinColSelect("Select Targets");
+
+        // Put some items in the select
+        twc.addItems("Mercury", "Venus", "Earth", "Mars",
+        "Jupiter", "Saturn", "Uranus", "Neptune");
+
+        // Few items, so we can set rows to match item count
+        twc.setRows(twc.size());
+
+        // Preselect a few items by creating a set
+        HashSet<String> hs = new  HashSet<String>( Arrays.asList("Mercury", "Venus", "Earth", "Mars",
+        "Jupiter", "Saturn", "Uranus", "Neptune"));
+        twc.setValue(hs);
+
+        // Handle value changes
+        twc.addValueChangeListener(event -> // Java 8
+        layout.addComponent(new Label("maPlaylist: " + event.getProperty().getValue())));
+        hol.addComponent(twc);
+        
+
+        
+
+        gestionBarre gb = new gestionBarre() ;
+        rechercheField.addListener(gb);
+        rechercher.addClickListener( callback ) ;
+        
+
+         layout.addComponent(hol);
+         layout.setMargin(true);
+         layout.setSpacing(true);
         
         setContent(layout);
     }
@@ -85,4 +160,9 @@ public class MyUI extends UI {
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
+
 }
+ 
+
+
+
